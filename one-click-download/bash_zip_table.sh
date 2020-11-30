2bb80fd43c0cce284410e917682e27bc598d9120
Fred Israel <fredpublico@gmail.com>
2020-11-30 20:29:23 -0300

    added bash version, much faster

--- one-click-download/bash_zip_table.sh
+++ one-click-download/bash_zip_table.sh
@@ -12,6 +12,8 @@ if [[ $LIMIT ]]; then LIMIT="LIMIT ${LIMIT}"; fi
 
 
 gsutil -m rm -r "gs://basedosdados-public/tmp/to_zip/$DATASET/$TABLE/" || true
+trap "gsutil -m rm -r 'gs://basedosdados-public/tmp/to_zip/$DATASET/$TABLE/' || true" EXIT
+
 bq query --nouse_legacy_sql <<EOF
     EXPORT DATA OPTIONS(
         uri="gs://basedosdados-public/tmp/to_zip/$DATASET/$TABLE/*.csv",
@@ -27,15 +29,14 @@ bq query --nouse_legacy_sql <<EOF
 EOF
 
 BLOB_PATH="one-click-download/$DATASET/$TABLE.zip" # todo add {version}
-# FILE_NAME="${TABLE}.csv"
-# mkfifo $FILE_NAME
-mkfifo upload
-mkfifo byte_count
-mkfifo counter
-gsutil cp "gs://basedosdados-public/tmp/to_zip/$DATASET/$TABLE/*" - | tee counter | gzip - | tee upload byte_count > /dev/null &
+FILE_NAME="${TABLE}.csv"
+mkfifo $FILE_NAME upload byte_count counter
+gsutil cp "gs://basedosdados-public/tmp/to_zip/$DATASET/$TABLE/*" - | tee counter > $FILE_NAME &
+zip --fifo - $FILE_NAME | tee upload > byte_count &
 wc --bytes byte_count > bytes_written &
-gsutil cp upload gs://basedosdados-public/$BLOB_PATH &
-perl -nE 'say $. if ($. % 200000 == 0);' counter
-gsutil ls -l gs://basedosdados-public/$BLOB_PATH
+gsutil cp - gs://basedosdados-public/$BLOB_PATH < upload &
+perl -nE 'say $. if ($. % 200000 == 0);' < counter
 wait
+gsutil ls -l gs://basedosdados-public/$BLOB_PATH
 echo "Written $(cat bytes_written) bytes"
+echo Done
--- one-click-download/handler.py
+++ one-click-download/handler.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
 import bottle
 from zip_table import zip_full_table_and_store
-import json
+import json, subprocess
 
 app = bottle.Bottle()
 
 @app.route('/', method='POST')
-def hello():
-    args = bottle.request.json
-    output = zip_full_table_and_store(args['dataset'], args['table'], args.get('limit'))
+def zip_bash():
+    body = bottle.request.json
+    args = ['/app/bash_zip_table.sh', body['dataset'], body['table'],]
+    if body.get('limit'): args.append(body['limit'])
+    output = subprocess.run(args, capture_output=False, encoding='utf8')
+    return {'output': 'ok'}
+
+@app.route('/python', method='POST')
+def zip_python():
+    body = bottle.request.json
+    output = zip_full_table_and_store(body['dataset'], body['table'], body.get('limit'))
     return {'output': output}
 
 @app.error()
