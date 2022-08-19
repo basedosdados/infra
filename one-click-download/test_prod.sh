d61bf15c1e1de7ab01c25c35c4a70f20ba64eb01
polvoazul <fredpublico@gmail.com>
2022-08-19 15:54:15 -0300

    Update test_prod.sh

--- one-click-download/test_prod.sh
+++ one-click-download/test_prod.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -e
 
+DATASET=${1:-br_ms_sim}
+TABLE=${2:-municipio_causa_idade_genero_raca}
+
 http --timeout=300 post https://zip-full-table-6op2ytwc6q-ue.a.run.app \
         "Authorization:Bearer $(gcloud auth print-identity-token)" \
-        dataset=br_ms_sim table=municipio_causa_idade_genero_raca limit=4500 debug=true
+        dataset=$DATASET table=$TABLE limit=4500 debug=true
 
-echo gs://basedosdados-public/test/one-click-download/br_ms_sim/municipio_causa_idade_genero_raca.zip
-gsutil ls -l  gs://basedosdados-public/test/one-click-download/br_ms_sim/municipio_causa_idade_genero_raca.zip
+echo gs://basedosdados-public/test/one-click-download/$DATASET/$TABLE.zip
+gsutil ls -l  gs://basedosdados-public/test/one-click-download/$DATASET/$TABLE.zip
